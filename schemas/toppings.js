fc8b0eb383bf6676da6950791d352efdc2c65892
Jimmy Wang <jawang12@gmail.com>
2021-03-15 16:38:56 -0400

    toppings field in sanity

---
+++ schemas/toppings.js
@@ -0,0 +1,33 @@
+import { FaPepperHot } from 'react-icons/fa';
+
+export default {
+  title: 'Toppings',
+  name: 'toppings',
+  type: 'document',
+  icon: FaPepperHot,
+  fields: [
+    {
+      title: 'Name of Topping',
+      name: 'name',
+      type: 'string',
+      description: 'Name of topping'
+    },
+    {
+      title: 'Vegetarian',
+      name: 'vegetarian',
+      type: 'boolean',
+      options: {
+        layout: 'checkbox'
+      }
+    }
+  ], 
+  preview: {
+    select: {
+      name: 'name',
+      vegan: 'vegetarian' // selected value based off name prop in line 17
+    },
+    prepare: ({ name, vegan }) => ({
+      title: vegan && name + ' (v)' || name
+    })
+  }
+}
